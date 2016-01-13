d78c97339841d03959b98d6ab756826951edbd31
Spyros Anastasopoulos <winwasher@gmail.com>
2016-01-13 23:39:48 +0200

    add dns cache

--- downloader/downloader.go
+++ downloader/downloader.go
@@ -12,13 +12,89 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"sync"
 )
 
 var finPath = flag.String("fin", "urls4.txt", "urls file path")
+var nDnsResolvers = flag.Int("wdns", 4, "number of dns workers")
+
+type DnsCache struct {
+	sync.Mutex
+	hosts map[string]string
+}
+
+type DnsResponse struct {
+	addr string
+	err  error
+}
+
+type DnsRequest struct {
+	host  string
+	respc chan DnsResponse
+}
+
+var defaultDnsCache = DnsCache{hosts: make(map[string]string)}
+var dnsResolveChannel = make(chan DnsRequest)
+
+func DnsResolver(c chan DnsRequest) {
+	for {
+		req, ok := <-c
+		if !ok {
+			return
+		}
+
+		defaultDnsCache.Lock()
+		addr, ok := defaultDnsCache.hosts[req.host]
+		defaultDnsCache.Unlock()
+
+		if ok {
+			req.respc <- DnsResponse{addr, nil}
+		} else {
+			addrs, err := net.LookupHost(req.host)
+			if err == nil {
+				defaultDnsCache.Lock()
+				defaultDnsCache.hosts[req.host] = addrs[0]
+				defaultDnsCache.Unlock()
+				log.Println("Resolved", req.host, "to", addrs[0])
+				req.respc <- DnsResponse{addr: addrs[0]}
+			} else {
+				req.respc <- DnsResponse{err: err}
+			}
+		}
+	}
+}
+
+func ResolvingDial(network, addr string) (net.Conn, error) {
+	host, port, err := net.SplitHostPort(addr)
+	if err != nil {
+		return nil, err
+	}
+
+	var final_addr = host
+	if ip_addr := net.ParseIP(host); ip_addr == nil {
+		req := DnsRequest{host: host, respc: make(chan DnsResponse)}
+		dnsResolveChannel <- req
+		resp := <-req.respc
+
+		if resp.err != nil {
+			return nil, err
+		} else {
+			final_addr = resp.addr
+		}
+	}
+
+	log.Printf("Opening connection: %s!%s", network, final_addr)
+	return net.Dial(network, net.JoinHostPort(final_addr, port))
+}
 
 func main() {
 	log.SetFlags(log.Lshortfile)
 	flag.Parse()
+
+	defer close(dnsResolveChannel)
+	for i := 0; i < *nDnsResolvers; i++ {
+		go DnsResolver(dnsResolveChannel)
+	}
 
 	fin, err := os.Open(*finPath)
 	if err != nil {
@@ -54,13 +130,8 @@ func main() {
 	}
 }
 
-func LoggingDial(network, addr string) (net.Conn, error) {
-	log.Printf("Opening connection: %s!%s", network, addr)
-	return net.Dial(network, addr)
-}
-
 func DownloaderForHost(host string, endCh chan int) chan string {
-	client := http.Client{Transport: &http.Transport{Dial: LoggingDial}}
+	client := http.Client{Transport: &http.Transport{Dial: ResolvingDial}}
 	hasher := sha1.New()
 
 	c := make(chan string)
